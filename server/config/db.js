b1502e3b5f1a2e499d6a504fd4d13e09571cf2ec
Abissalaurent <laurentabissa@gmail.com>
2025-10-21 20:41:12 +0000

    mise a jour backend

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI);
-//     console.log("MongoDB connecté");
-//   } catch (err) {
-//     console.error("Erreur de connexion à MongoDB:", err.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
-
 // config/db.js
 const mongoose = require('mongoose');
 
